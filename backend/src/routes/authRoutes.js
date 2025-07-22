9d40d50118fc486a90b1939055734b5a4842bad3
Krish <krishna.official0717@gmail.com>
2025-07-22 06:55:00 +0530

    feat: Add authRoutes.js implementation

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+} from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+router.post("/register", registerUser);
+router.post("/login", authUser);
+router.post("/logout", logoutUser);
+export default router;
